b7bac6e13e22ea8a31c2220b45e1d09777c6864d
didsqq <adelgaripov6@gmail.com>
2025-01-11 21:59:25 +0300

    отправка статей в тг воркает

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/didsqq/news_feed_bot/internal/bot"
 	"github.com/didsqq/news_feed_bot/internal/botkit"
 	"github.com/didsqq/news_feed_bot/internal/config"
 	"github.com/didsqq/news_feed_bot/internal/fetcher"
@@ -17,6 +18,7 @@ import (
 	"github.com/didsqq/news_feed_bot/internal/summary"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -28,7 +30,7 @@ func main() {
 
 	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
 	if err != nil {
-		log.Printf("failed to connect to databas: %v", err)
+		log.Printf("failed to connect to database: %v", err)
 		return
 	}
 	defer db.Close()
@@ -58,6 +60,7 @@ func main() {
 	)
 
 	newsBot := botkit.New(botAPI)
+	newsBot.RegisterCmdView("start", bot.ViewCmdStart())
 
 	mux := http.NewServeMux() // создает обработчик путей
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -87,15 +90,15 @@ func main() {
 		}
 	}(ctx)
 
-	go func(ctx context.Context) {
-		if err := http.ListenAndServe("9.0.0.0:8080", mux); err != nil { // запускает http-сервер, слушает порт 8080, использует маршрутизатор mux
-			if !errors.Is(err, context.Canceled) {
-				log.Printf("[ERROR] failed to run http server: %v", err)
-				return
-			}
-			log.Printf("[INFO] http server stopped")
-		}
-	}(ctx)
+	// go func(ctx context.Context) {
+	// if err := http.ListenAndServe("9.0.0.0:8080", mux); err != nil { // запускает http-сервер, слушает порт 8080, использует маршрутизатор mux
+	// 	if !errors.Is(err, context.Canceled) {
+	// 		log.Printf("[ERROR] failed to run http server: %v", err)
+	// 		return
+	// 	}
+	// 	log.Printf("[INFO] http server stopped")
+	// }
+	// }(ctx)
 
 	if err := newsBot.Run(ctx); err != nil {
 		log.Printf("[ERROR] failed to run botkit: %v", err)
